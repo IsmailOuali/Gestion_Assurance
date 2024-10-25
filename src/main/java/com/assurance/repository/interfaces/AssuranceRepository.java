c7b228f3a9aa83d3ba12ec3f562ad87233c3496d
unknown <ismail.ouali@ump.ac.ma>
2024-10-25 10:52:03 +0100

    Fixing bugs

--- src/main/java/com/assurance/repository/interfaces/AssuranceRepository.java
+++ src/main/java/com/assurance/repository/interfaces/AssuranceRepository.java
@@ -1,9 +1,9 @@
 package com.assurance.repository.interfaces;
 
-import com.assurance.model.AssuranceAutomobile;
+import com.assurance.model.Assurance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface AssuranceRepository extends JpaRepository<AssuranceAutomobile, UUID> {
+public interface AssuranceRepository extends JpaRepository<Assurance, UUID> {
 }
