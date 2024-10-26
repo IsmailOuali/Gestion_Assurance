c3bc9c3de2c8412d8d8ed5cf2929ba0506b48c9a
unknown <ismail.ouali@ump.ac.ma>
2024-10-26 23:11:40 +0100

    Adding service Calculator

--- src/main/java/com/assurance/controller/AssuranceController.java
+++ src/main/java/com/assurance/controller/AssuranceController.java
@@ -3,17 +3,23 @@ package com.assurance.controllers;
 import com.assurance.model.AssuranceSante;
 import com.assurance.model.AssuranceAutomobile;
 import com.assurance.model.AssuranceHabitation;
+import com.assurance.model.Devis;
+import com.assurance.services.implementations.CalculateurDevis;
 import com.assurance.services.interfaces.AssuranceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/assurance")
 public class AssuranceController {
 
     private final AssuranceService assuranceService;
+
+    @Autowired
+    private CalculateurDevis calculateurDevis;
 
     @Autowired
     public AssuranceController(AssuranceService assuranceService) {
@@ -26,20 +32,29 @@ public class AssuranceController {
     }
 
     @PostMapping("/sante")
-    public String addAssuranceSante(@ModelAttribute AssuranceSante assuranceSante) {
+    public ModelAndView addAssuranceSante(@ModelAttribute AssuranceSante assuranceSante) {
         assuranceService.addAssuranceSante(assuranceSante);
-        return "redirect:/assurance/form"; // Redirect back to the form after submission
+        Devis devis = calculateurDevis.calculerDevis(assuranceSante);
+        ModelAndView mav = new ModelAndView("devis");
+        mav.addObject("devis", devis);
+        return mav;
     }
 
     @PostMapping("/habitation")
-    public String addAssuranceHabitation(@ModelAttribute AssuranceHabitation assuranceHabitation) {
+    public ModelAndView addAssuranceHabitation(@ModelAttribute AssuranceHabitation assuranceHabitation) {
         assuranceService.addAssuranceHabitation(assuranceHabitation);
-        return "redirect:/assurance/form"; // Redirect back to the form after submission
+        Devis devis = calculateurDevis.calculerDevis(assuranceHabitation);
+        ModelAndView mav = new ModelAndView("devis");
+        mav.addObject("devis", devis);
+        return mav;
     }
 
     @PostMapping("/automobile")
-    public String addAssuranceAutomobile(@ModelAttribute AssuranceAutomobile assuranceAutomobile) {
+    public ModelAndView addAssuranceAutomobile(@ModelAttribute AssuranceAutomobile assuranceAutomobile) {
         assuranceService.addAssuranceAutomobile(assuranceAutomobile);
-        return "redirect:/assurance/form"; // Redirect back to the form after submission
+        Devis devis = calculateurDevis.calculerDevis(assuranceAutomobile);
+        ModelAndView mav = new ModelAndView("devis");
+        mav.addObject("devis", devis);
+        return mav;
     }
 }
